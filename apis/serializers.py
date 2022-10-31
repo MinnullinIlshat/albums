7e9e065f5a62a69e98116ccb6e1fde548e84f9e7
Ilshat Minnullin <minnullinilshat@gmail.com>
2022-10-31 17:54:19 +0300

    311022 even

--- apis/serializers.py
+++ apis/serializers.py
@@ -1,9 +1,31 @@
 from rest_framework import serializers 
 
-from albums.models import Album
+from albums.models import Album, Artist, Track
 
+class ArtistSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Artist 
+        fields = ("name",)
+
+class TrackSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Track 
+        fields = ("name", "album")
 
 class AlbumSerializer(serializers.ModelSerializer):
+    album = serializers.SerializerMethodField()
+    artist = ArtistSerializer()
+    tracks = serializers.SerializerMethodField()
+
+    def get_tracks(self, album):
+        return get_tracks(album)
+
+    def get_album(self, album):
+        return str(album)
+
     class Meta:
         model = Album 
-        fields = ("name", "artist", "year")
+        fields = ("album", "name", "artist", "tracks")
+
+def get_tracks(album):
+    return album.track_set.all()
