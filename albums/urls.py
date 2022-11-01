71a9d18c3d78016ca21d8edb83f877944522630c
Ilshat Minnullin <minnullinilshat@gmail.com>
2022-11-01 16:57:52 +0300

    albums/views.py completed

--- albums/urls.py
+++ albums/urls.py
@@ -6,4 +6,6 @@ from .views import index
 
 urlpatterns = [ 
     path('', index, name='home'),
+    path('sort_by/<str:sort_by>/', index, name='sort_by_album_name'),
+    path('sort_by/<str:sort_by>/', index, name='sort_by_artist_name')
 ]
--- albums/views.py
+++ albums/views.py
@@ -3,6 +3,12 @@ from django.shortcuts import render
 import requests 
 
 
-def index(request):
+def index(request, sort_by=None):
     responses = requests.get('http://127.0.0.1:8000/api/').json()
+    if sort_by == 'album_name':
+        key = lambda x: x['name']
+        responses = sorted(responses, key=key)
+    elif sort_by == 'artist_name':
+        key = lambda x: x['artist']['name']
+        responses = sorted(responses, key=key)
     return render(request, 'albums/index.html', {'responses': responses})
