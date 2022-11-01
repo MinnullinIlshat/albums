94a09a983e80648e416e01a7227675c882ec071b
Ilshat Minnullin <minnullinilshat@gmail.com>
2022-11-01 17:31:48 +0300

    completed 01_11_22

--- albums/urls.py
+++ albums/urls.py
@@ -6,6 +6,6 @@ from .views import index
 
 urlpatterns = [ 
     path('', index, name='home'),
-    path('sort_by/<str:sort_by>/', index, name='sort_by_album_name'),
-    path('sort_by/<str:sort_by>/', index, name='sort_by_artist_name')
+    path('sort_by/<str:sort_by>/', index, name='sort_by'),
+    #path('sort_by/<str:sort_by>/', index, name='sort_by_artist_name')
 ]
